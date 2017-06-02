2738b82fbf87574bce691e9265c44ddb109a11e5
Matthew Brown <mattd429@gmail.com>
2017-06-03 07:50:15 +0900

    Update and rename WatchfaceSync to WatchfaceSync.java

---
+++ commons/src/main/java/com.customwatchface.mbrown.commons/WatchfaceSync.java
@@ -0,0 +1,14 @@
+package com.customewatchface.mbrown.commons
+
+/** 
+  * shared on Github 6/3/17
+  **/
+  
+  public class WatchfaceSyncCommons {
+    private WatchfaceSynccommons() {
+    }
+    
+    public static final String PATH = "/custom_watch_face_config";
+    public static final String KEY_BACKGROUND_COLOUR = "KEY_BACKGROUND_COLOUR";
+    public static final String KEY_DATE_TIME_COLOUR = "KEY_DATE_TIME_COLOUR";
+}
