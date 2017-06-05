098081db88e59ea46fd2c78fced1da8a8d1fd272
Matthew Brown <mattd429@gmail.com>
2017-06-05 12:17:23 +0900

    Update ApplicationTest.java

--- mobile/src/androidTest/java/com.customwatchface.mbrown.customwatchface/ApplicationTest.java
+++ mobile/src/androidTest/java/com.customwatchface.mbrown.customwatchface/ApplicationTest.java
@@ -1,1 +1,18 @@
+package com.customwatchface.mbrown.customwatchface;
 
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+  * uploaded to Github by MBrown 6/5/2017.
+  */
+
+/**
+  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+  */
+
+public class  ApplicationTest extends ApplicationTestCase<Application>{
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
