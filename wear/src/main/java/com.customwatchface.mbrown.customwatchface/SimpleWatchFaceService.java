83b26a8ec5384cb5e35dfcbb97ec1e0002aa6427
Matthew Brown <mattd429@gmail.com>
2017-06-27 19:38:56 -0400

    Update SimpleWatchFaceService.java

--- wear/src/main/java/com.customwatchface.mbrown.customwatchface/SimpleWatchFaceService.java
+++ wear/src/main/java/com.customwatchface.mbrown.customwatchface/SimpleWatchFaceService.java
@@ -1,1 +1,35 @@
+package com.customwatchface.mbrown.customwatchface;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+import com.customwatcface.mbrown.commons.WatchfaceSyncCommons;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearbale.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearbale.DataItem;
+import com.google.android.gms.wearbale.DataItemBuffer;
+import com.google.android.gms.wearble.DataMap;
+import com.google.android.gms.wearbale.DataMapItem;
+import com.google.android.gms.wearbale.Wearable;
+
+
+import java.util.concurrent.TimeUnit;
